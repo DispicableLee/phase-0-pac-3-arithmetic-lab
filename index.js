0772b0fcaaa63be919bf9414b84095e1cc5ef2e8
DispicableLee <lee.robert053@gmail.com>
2022-08-11 13:28:24 +0800

    done

---
+++ index.js
@@ -0,0 +1,62 @@
+function add(a,b){
+    const c = a + b;
+    return c;
+}
+
+function subtract(a,b){
+    const c = a - b;
+    return c;
+}
+
+function multiply(a,b){
+    const c = a*b;
+    return c;
+}
+
+function divide(a,b){
+    const c = a/b;
+    return c;
+}
+
+function add(a,b){
+    const c = a + b;
+    return c;
+}
+
+function subtract(a,b){
+    const c = a - b;
+    return c;
+}
+
+function multiply(a,b){
+    const c = a*b;
+    return c;
+}
+
+function divide(a,b){
+    const c = a/b;
+    return c;
+}
+
+function increment(a){
+    const c = a + 1;
+    return c;
+}
+
+function decrement(a){
+    const c = a - 1;
+    return c;
+}
+
+function makeInt(n){
+    const c = n.toString();
+    if(c!=n.toString()){
+        return null
+    }else{
+        return c
+    };
+}
+
+function preserveDecimal(n){
+    return n.parseFloat();
+}
